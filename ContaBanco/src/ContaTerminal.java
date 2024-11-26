8716f09246c6698821a774baf8c8767ac5549d35
Daniel <dansampaio977@gmail.com>
2024-11-26 14:39:38 -0300

    remoção dos comentários para limpar o código

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,10 +4,10 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Entrada dos dados
+        
         System.out.print("Por favor, digite o número da conta: ");
         int numero = scanner.nextInt();
-        scanner.nextLine(); // Consumir o "\n"
+        scanner.nextLine(); 
 
         System.out.print("Por favor, digite o número da agência: ");
         String agencia = scanner.nextLine();
@@ -18,7 +18,7 @@ public class ContaTerminal {
         System.out.print("Por favor, digite o saldo da conta: ");
         double saldo = scanner.nextDouble();
 
-        // Exibição da mensagem final com formatação para valores monetários
+        
         System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
         System.out.printf("Sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque.%n",  agencia, numero, saldo);
 
